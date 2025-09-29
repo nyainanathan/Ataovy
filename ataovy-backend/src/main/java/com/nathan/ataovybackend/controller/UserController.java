bcfa25f9f028d63a1142534ccc386a0fa1f46295
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-09-29 13:25:13 +0300

    feat: getting user by email

--- ataovy-backend/src/main/java/com/nathan/ataovybackend/controller/UserController.java
+++ ataovy-backend/src/main/java/com/nathan/ataovybackend/controller/UserController.java
@@ -4,10 +4,12 @@ import com.nathan.ataovybackend.model.User;
 import com.nathan.ataovybackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -18,5 +20,10 @@ public class UserController {
     @GetMapping("/")
     public List<User> getAllUser() {
         return service.getAllUser();
+    }
+
+    @GetMapping("/{email}")
+    public Optional<User> getUserByEmail(@PathVariable String email) {
+        return service.getUserByEmail(email);
     }
 }
--- ataovy-backend/src/main/java/com/nathan/ataovybackend/service/UserService.java
+++ ataovy-backend/src/main/java/com/nathan/ataovybackend/service/UserService.java
@@ -6,13 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepo repo;
+
     public List<User> getAllUser() {
         return repo.findAll();
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return repo.findByEmail(email);
     }
 }
