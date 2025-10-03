2f82b94f6156db49cad5bc95c375b477af4a4c79
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-10-03 19:52:17 +0300

    feat: login route returning a jwt

--- ataovy-backend/src/main/java/com/nathan/ataovybackend/controller/AuthController.java
+++ ataovy-backend/src/main/java/com/nathan/ataovybackend/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.nathan.ataovybackend.controller;
 
+import com.nathan.ataovybackend.dto.LoginRequest;
+import com.nathan.ataovybackend.model.User;
 import com.nathan.ataovybackend.security.JwtUtil;
+import com.nathan.ataovybackend.service.AuthService;
 import io.jsonwebtoken.JwtException;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -13,6 +16,12 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private JwtUtil jwtUtil;
+    private AuthService authService;
+
+    @PostMapping("/login")
+    private String loginUser(@RequestBody LoginRequest loginCredentials) {
+        return authService.loginUser(loginCredentials);
+    }
 
     @GetMapping("/token")
     public String getToken(@RequestBody String username) {
---
+++ ataovy-backend/src/main/java/com/nathan/ataovybackend/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.nathan.ataovybackend.service;
+
+import com.nathan.ataovybackend.dto.LoginRequest;
+import com.nathan.ataovybackend.model.User;
+import com.nathan.ataovybackend.repository.UserRepo;
+import com.nathan.ataovybackend.security.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private UserRepo repo;
+    private JwtUtil jwtUtil;
+    private PasswordEncoder passwordEncoder;
+
+    public String loginUser(LoginRequest loginCredentials) {
+        User user = repo.findByEmail(loginCredentials.getEmail());
+        if(user == null){
+            return "invalid email";
+        }
+        if(passwordEncoder.matches(loginCredentials.getPassword(), user.getPassword())){
+            String token = jwtUtil.generateToken(user.getEmail());
+            return token;
+        }
+        return "invalid password";
+    }
+}
