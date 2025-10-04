07b3cd3e8b37d7f718e9dc566a8274f32c19ad7e
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-10-04 16:01:03 +0300

    Feat: todo entity

---
+++ ataovy-backend/src/main/java/com/nathan/ataovybackend/model/Todo.java
@@ -0,0 +1,55 @@
+package com.nathan.ataovybackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name="todo")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+
+
+    enum ToDoStatus{
+        DONE, IN_PROGRESS, NOT_STARTED
+    }
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name="UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id",updatable = false, nullable = false,columnDefinition = "UUID")
+    private UUID id;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="deadline")
+    private LocalDateTime deadline;
+
+    @Column(name="is_recurring")
+    private Boolean isRecurring;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="status")
+    private ToDoStatus status;
+
+    @Column(name="user_id")
+    private UUID userId;
+
+    @Column(name="main_todo")
+    private UUID mainTodoId;
+
+}
