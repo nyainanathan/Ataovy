b554fb90d7747781554e663427ade54a653f3088
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-10-10 09:20:44 +0300

    Feat: put the to do into the next state

--- ataovy-frontend/src/Components/HomePage/Tasks/Task.jsx
+++ ataovy-frontend/src/Components/HomePage/Tasks/Task.jsx
@@ -1,13 +1,29 @@
 import { useEffect, useState } from "react";
 
 const Task = ({task}) => {
-
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May' , 'June' , 'July' , 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
     const [datee, setDatee] = useState('');
 
-    const getFormattedDate = () => {
-        //console.log(task);
-        
+    const API_URL = import.meta.env.VITE_BACKEND_URL;
+
+    const getNextState = () => {
+        if (task.status == 'NOT_STARTED') return 'IN_PROGRESS';
+        else if (task.status == 'IN_PROGRESS') return 'DONE';
+        else return 'ARCHIVED';
+    }
+    
+    const getNextStateButton = () => {
+        if(getNextState() == 'IN_PROGRESS') return 'START'
+        else if(getNextState() == 'DONE') return 'FINISH'
+        else return 'ARCHIVE'
+    }
+
+    const getPreviousState = () => {
+        if (task.status == 'IN_PROGRESS') return 'NOT_STARTED'
+        else return 'IN_PROGRESS'
+    }
+
+    const getFormattedDate = () => {    
         const date = task.deadline.substring(0,10).split('-').map(val => Number(val)).reverse();
         let formatedDate = months[date[1] - 1 ] + ' ' + date[0] + ', ' + date[2];
         console.log(formatedDate);
@@ -15,21 +31,63 @@ const Task = ({task}) => {
         
     }
 
+    const handleNextState = async () => {
+        try{
+            const editedTodo = task;
+            editedTodo.status = getNextState();
+            const editState = await fetch(`${API_URL}/todo/`, {
+                method: 'PUT',
+                credentials: 'include',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(editedTodo)
+            });
+            if(editState.ok) {
+                alert('To do edit succesfully')
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     useEffect(() => {
         getFormattedDate()
     } , [task])
 
     return (
-        <div className="bg-amber-200 mt-1 mb-1 p-3 rounded-2xl flex flex-col">
+        <div className="bg-amber-400 mt-1 mb-1 p-3 rounded-2xl flex flex-col">
             <div
                 className="flex items-center gap-3"
-            ><i className="fa-solid fa-calendar-days"></i>
+            >
+                <i className="fa-solid fa-calendar-days"></i>
                 <p>{datee}</p>
+           
             </div>
-            <p>{task.description}</p>
+            <div className="flex justify-between">
+
+            <p className="p-2">
+                {task.description}
+            </p>
+            <div className="flex flex-col gap-2">
+                {
+                    task.status != 'NOT_STARTED' && (
+                        <i className="fa-solid fa-circle-arrow-left text-2xl"></i>
+                    )
+                    
+                }
+                <i className="fa-solid fa-circle-arrow-right text-2xl"></i>
+            </div>
+            </div>
             <div className="flex gap-2 m-auto">
-                <button className="bg-green-400 p-2 rounded-2xl">Done</button>
-                <button className="bg-blue-400 p-2 rounded-2xl">Edit</button>
+                <button className="bg-green-400 p-2 rounded-2xl"
+                    onClick={handleNextState}
+                >
+                    {
+                            getNextStateButton()
+                    }
+                </button>
+                <button className="bg-blue-400 p-2 rounded-2xl">EDIT</button>
             </div>
         </div>
     )
